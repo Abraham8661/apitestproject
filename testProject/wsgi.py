1bac836fa78e8003d93ef7d6aa8a024e3601ace9
Abraham <abrahamedet9@gmail.com>
2024-07-13 16:50:46 +0100

    Static files fix

--- testProject/wsgi.py
+++ testProject/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project_name.settings')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
+
